eba755b7245c20cf38d76238e3d33f9433dfc39d
Saurabh Kumar <asksaurabh101@gmail.com>
2022-08-07 23:47:28 +0530

    Add response format of our api and route for our api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # API definition
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     # We are going to list our resources here
   end
 end

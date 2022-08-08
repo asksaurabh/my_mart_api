e1acff71abaf4839668db30ff035e0e9fb356adf
Saurabh Kumar <asksaurabh101@gmail.com>
2022-08-08 23:04:05 +0530

    Add create action to respond to POST request to create a new user

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -13,4 +13,24 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(self.response.body)
     assert_equal @user.email, json_response["email"]
   end
+
+  test "should create new user" do
+    new_email = "kk@gmail.com"
+    password = "1234"
+    assert_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: new_email, 
+                                              password: password }}, as: :json
+    end
+    assert_response :created
+  end
+
+  test "should not create a new user wit taken email" do
+    new_email = @user.email
+    password = "1234"
+    assert_no_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: new_email, 
+                                              password: password }}, as: :json
+    end
+    assert_response :unprocessable_entity
+  end
 end

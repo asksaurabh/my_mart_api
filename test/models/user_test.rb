d2c2ff9dc4a886dce5f4dda0bfbf97c560eb6229
Saurabh Kumar <asksaurabh101@gmail.com>
2022-08-08 20:59:29 +0530

    Add fixtures and test for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:saurabh)
+  end
+  
+  test "user with a valid email should be valid" do
+    assert @user.valid?
+  end
+
+  test "user with invalid email should be invalid" do
+    @user.email = "happpygmail.com"
+    assert_not @user.valid?
+  end
+
+  test "user with taken email should be invalid" do
+    other_user = users(:saurabh)
+    @user = User.new(email: other_user.email, password_digest: "hhdh")
+    assert_not @user.valid?
+  end
 end

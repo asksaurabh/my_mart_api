0235b9d340cf7a1e14bb1b082ceb6039c3b0b0f9
Saurabh Kumar <asksaurabh101@gmail.com>
2022-08-10 00:47:03 +0530

    Add index action to list all products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::ProductsController < ApplicationController
 
+  # GET /products/1
   def show
     render json: Product.find(params[:id])
   end
-  
+
+  # GET /products
+  def index
+    render json: Product.all 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:show, :create, :update, :destroy]
       resources :tokens, only: [:create]
-      resources :products, only: %i[show]
+      resources :products, only: %i[show index]
     end
   end
 end

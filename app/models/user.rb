926f1095f2105be5d221d3be07bf50e924712141
Saurabh Kumar <asksaurabh101@gmail.com>
2022-08-10 23:20:17 +0530

    Add Order model and establish the relationship with User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
   validates :email, presence: true, length: { maximum: 255 }, 
                     format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :password_digest, presence: true 
